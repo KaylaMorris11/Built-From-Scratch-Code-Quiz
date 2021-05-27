6c9cbdf6437ae7c7ddca84762a96d5ebc9e27143
Kayla Morris <moreiskay14@gmail.com>
2021-05-27 16:23:25 -0700

    Starter html css and javascript code

---
+++ script.js
@@ -0,0 +1,14 @@
+var quizStart = document.getElementById('quiz-start');
+var questionContainer = document.getElementById("question-container")
+var startTimer = 120000
+var countdownEl = document.getElementById("countdown")
+
+var quizTimer = setInterval(quizStart, 120000)
+
+
+
+quizStart.addEventListener("click", startTheQuiz);
+
+function startTheQuiz() {
+   
+};
