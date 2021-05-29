ac9bd1e6f3a3df007359589ef1843397943373a8
Kayla Morris <moreiskay14@gmail.com>
2021-05-29 13:43:46 -0700

    Added a couple of questions and got my start button to show and hide my questions

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
-var quizStart = document.getElementById('quiz-start');
-var questionContainer = document.getElementById("question-container")
-var startTimer = 120000
-var countdownEl = document.getElementById("countdown")
-
-var quizTimer = setInterval(quizStart, 120000)
+var startTheQuiz = document.getElementById("start-quiz");
 
 
+var timeEl = document.querySelector(".timer");
 
-quizStart.addEventListener("click", startTheQuiz);
+var timeToFinish = 75;
+//Start button
+// startTheQuiz.addEventListener("click", startQuiz)
 
-function startTheQuiz() {
-   
-};
+//if else statement content source from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+function startQuiz() {
+
+    var question1 = document.getElementById("question1")
+    question1.style.display = "inline";
+    var hide = document.getElementById("title-page")
+    hide.style.display = "none";
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        timeToFinish--;
+        timeEl.textContent = "Time:" + timeToFinish;
+
+        if (secondsLeft === 0) {
+
+            clearInterval(timerInterval);
+        }
+
+    }, 75000);
+}
+
+
+// var title = document.getElementById("title-page");
+//     if (title.style.display === "inline") {
+//         title.style.display = "none";
+//       } else {
+//         title.style.display = "none";
+//   }
+//     var question1 = document.getElementById("question1-box");
+//     if (question1.style.display === "none") {
+//       question1.style.display = "inline";
+//     } else {
+//       question1.style.display = "none";
+//     }
+//     var question2 = document.getElementById("question2-box");
+//     if (question2.style.display === "inline") {
+//       question2.style.display = "none";
+//     } else {
+//       question2.style.display = "inline";
+//     }
