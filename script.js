686768893a7e37490dc035ed168f21d5d465c5a2
Kayla Morris <moreiskay14@gmail.com>
2021-08-31 07:47:21 -0700

    Added an ability to record the score by the time left in the quiz

--- script.js
+++ script.js
@@ -1,68 +1,98 @@
 //Start of the quiz declarations and functions
 var timeEl = document.querySelector(".timer");
 var timeToFinish = 75;
+var timerInterval
+
+
 //Starts the timer when clicked and hides the code title page and shows Question 1
 function startQuiz() {
 
-    var question1 = document.getElementById("question1")
+    var question1 = document.getElementById("question1");
     question1.style.display = "inline";
     var hide = document.getElementById("title-page");
-    hide.style.display = "none";
-}
+    hide.style.display =
+        "none";
+};
+
 function setTime() {
-    var timerInterval = setInterval(function() {
-        timeToFinish --;
+    timerInterval = setInterval(function () {
+        timeToFinish--;
         timeEl.textContent = "Time:" + timeToFinish;
         if (timeToFinish === 0) {
-            clearInterval(timerInterval);
+            stopTimer();
         }
     }, 1000);
-}
+};
+
+function stopTimer() {
+    clearInterval(timerInterval);
+};
 
 timeEl.textContent = "Time:" + timeToFinish;
 
+function handleUserSelection(event) {
+    var usersChoice = event.target.id;
+    if (usersChoice === "correct") {
+        document.getElementById("displayCorrect").innerText = "Correct!"
+        //If user selects correct answer, then display "Correct!"
+    }
+    else {
+        //if user selects wrong answer take 5 seconds & display "Wrong!"
+        // timeToFinish = timeToFinish - 5;
+        timeToFinish -= 5;
+    };
+};
+
+function replayQuiz(event) {
+    event.preventDefault();
+    const username = document.getElementById("fname").value
+    var highScoresStringified = localStorage.getItem("codeQuiz") || [];
+    console.log(highScoresStringified);
+    var highScoresParsed = JSON.parse(highScoresStringified);
+    console.log(highScoresParsed);
+    var user = {
+        user: username,
+        score: timeToFinish,
+    }
+    highScoresParsed.push(user);
+    var stringifiedHighScores = JSON.stringify(highScoresParsed);
+    console.log(stringifiedHighScores);
+    localStorage.setItem("codeQuiz", stringifiedHighScores);
+
+    for(let i = 0; i < highScoresParsed.length; i++){
+        var user = highScoresParsed[i];
+        console.log(user)
+    }
+    //save user score then reset quiz
+};
+
+
 //When the user picks an answer it hides question 1 and brings to Question 2
-function questionChoices1() {
-var question2 = document.getElementById("question2")
-question2.style.display = "inline";
-var hide = document.getElementById("question1")
-hide.style.display = "none";
+function questionChoices1(event) {
+    handleUserSelection(event);
+    var question2 = document.getElementById("question2");
+    question2.style.display = "inline";
+    var hide = document.getElementById("question1");
+    hide.style.display = "none";
 };
 //Question 3 hide question 2
-function questionChoices2() {
-var question3 = document.getElementById("question3")
-question3.style.display = "inline";
-var hide = document.getElementById("question2")
-hide.style.display = "none";
+function questionChoices2(event) {
+    handleUserSelection(event);
+    var question3 = document.getElementById("question3");
+    question3.style.display = "inline";
+    var hide = document.getElementById("question2");
+    hide.style.display = "none";
 };
 
+function questionChoices3(event) {
+    handleUserSelection(event);
+    var displayScore = document.getElementById("displayScore")
+    displayScore.style.display = "inline";
+    stopTimer();
+    var userScore = document.getElementById("userScore");
+    console.log(timeToFinish)
+    userScore.textContent = timeToFinish;
+    var hide = document.getElementById("question3")
+    hide.style.display = "none";
+};
 
-
-
-
-
-//if else statement content source from https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-// var title = document.getElementById("title-page");
-//     if (title.style.display === "inline") {
-//         title.style.display = "none";
-//       } else {
-//         title.style.display = "none";
-//   }
-//     var question1 = document.getElementById("question1-box");
-//     if (question1.style.display === "none") {
-//       question1.style.display = "inline";
-//     } else {
-//       question1.style.display = "none";
-//     }
-//     var question2 = document.getElementById("question2-box");
-//     if (question2.style.display === "inline") {
-//       question2.style.display = "none";
-//     } else {
-//       question2.style.display = "inline";
-//     } 
-
-// function questionChoices() {
-//     if( (onclick.#choice1) {
-//         console.log(Wrong!)
-//     }
-// }
